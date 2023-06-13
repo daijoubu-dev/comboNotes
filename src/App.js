6d9f05653f156a261acf8246b4529e58239d7f83
TallGermanGuy <tallgermanguy@live.ca>
2023-06-12 21:25:11 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { nanoid } from 'nanoid';
+import NotesList from "./components/NotesList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      header: "This is my first Header",
+      text: "This is my first Note",
+      date: "12/06/2023"
+    },
+    {
+      id: nanoid(),
+      header: "This is my 2 Header",
+      text: "This is my 2 Note",
+      date: "13/06/2023"
+    },
+    {
+      id: nanoid(),
+      header: "This is my 3 Header",
+      text: "This is my 3 Note",
+      date: "14/06/2023"
+    },
+  ]);
+
+  const addNote = (header, text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      header: header,
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note)=> note.id !== id);
+    setNotes(newNotes);
+  }
+
+  return <div className="container"> 
+    <NotesList notes={notes} handleAddNote={addNote}/>
+  </div>
 }
 
 export default App;
---
+++ src/components/NotesList.js
@@ -0,0 +1,16 @@
+import AddNote from "./AddNote";
+import Note from "./Note";
+
+const NotesList = ({ notes, handleAddNote }) => {
+    return ( 
+    <div className='notes-list'>
+        <AddNote handleAddNote={handleAddNote}/>
+        {notes.map((note)=> (
+            <Note id={note.id} header={note.header} text={note.text} date={note.date}/>
+        ))}
+        
+    </div>
+    );
+};
+
+export default NotesList;
